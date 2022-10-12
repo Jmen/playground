27bd67a9930713b3c999d29e4d327a27c8471f2a
Jaimen Lathia <jaimenlathia@gmail.com>
2022-10-12 22:36:24 +0100

    feat: testing Vercel function logging

--- pages/api/error.ts
+++ pages/api/error.ts
@@ -9,6 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log("information", { hello: 'world' });
-  res.status(200).json({ name: 'John Doe' })
+  throw new Error("I'm an errror");
 }
